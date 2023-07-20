ab355c2ed1017780d3260bf97ad1e07d0c56052a
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-07-20 20:44:35 -0300

    Add responsividade nós botões

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="bg-slate-950 h-screen">
       <div className="flex justify-center" >
-        <Image className="mt-96"
+        <Image className="mt-32"
           src="/assets/hero.png" alt="Logo site"
           height={469}
           width={469}
@@ -12,14 +12,31 @@ export default function Home() {
         />
       </div>
       <h1
-          className="flex justify-center 
+        className="flex justify-center 
           text-center
         text-slate-50 
         mt-10 
         font-bold 
         text-2xl">
-        Sistema feito para você organizar <br/>seus estudos e tarefas
+        Sistema feito para você organizar <br />seus estudos e tarefas
       </h1>
+      <div className="sm:flex justify-center">
+        <div className="flex justify-center">
+          <button className="bg-slate-50 w-96 sm:bg-slate-50 sm:w-64 p-4 mt-8 rounded-lg font-bold text-lg"
+            type="submit">
+            + 7 posts
+          </button>
+
+        </div>
+        <div className="flex justify-center">
+          <button className="bg-slate-50 mt-8 flex justify-center w-96 sm:bg-slate-50 sm:w-64 sm:ml-10 p-4 rounded-lg font-bold text-lg"
+            type="submit">
+            + 7 Comentários
+          </button>
+
+        </div>
+
+      </div>
     </main>
   )
 }
