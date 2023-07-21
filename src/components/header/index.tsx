6b4b818d4147a4942e185b00080f9fa4a09507d7
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-07-21 07:55:29 -0300

    Add botão acessar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,20 @@
-
+import Link from "next/link"
 
 export default function Header() {
 
     return (
-        <header>
-            <section>
+        <header className="bg-slate-950">
+            <section className="flex justify-between">
                 <nav>
-                    <h1>Tarefas+</h1>
+                    <Link href="/">
+                        <h1 className="text-2xl font-bold p-3 ml-2 text-white ">Tarefas <span className="text-4xl font-extrabold text-red-500">+</span></h1>
+                    </Link>
+
                 </nav>
+
+                <button className="text-white mr-12 border border-solid 
+                rounded-3xl w-28 mt-3 font-bold hover:bg-white hover:text-black">Acessar</button>
+
             </section>
         </header>
     )
