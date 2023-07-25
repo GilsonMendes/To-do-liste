f92b44c363eac5811be782f10620f700c5068014
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-07-24 21:04:05 -0300

    Add transition nos botões do header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Home from "@/pages/home"
 import Header from "@/components/header"
+import Dashboard from "@/pages/dashboard"
 export default function App() {
   return (
     <>
       <Header />
       <Home />
+
     </>
   )
 }
