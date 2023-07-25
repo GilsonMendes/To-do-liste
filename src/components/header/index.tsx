d1fbc8e0d9536f68ca73777a29963cad60b13efa
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-07-24 21:05:00 -0300

    Add transition nos botões header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,16 +3,16 @@ import Link from "next/link"
 export default function Header() {
 
     return (
-        <header className="bg-slate-950">
+        <header className="bg-slate-950 h-0">
             <section className="flex justify-between">
                 <nav className="flex">
                     <Link href="/">
                         <h1 className="text-2xl font-bold p-3 ml-2 text-white ">Tarefas <span className="text-4xl font-extrabold text-red-500">+</span></h1>
                     </Link>
-                    <button className="text-black font-bold bg-white rounded-lg mt-3 p-3 hover:bg-slate-300 ">Meu Painel</button>
+                    <button className= "text-black font-bold bg-white rounded-lg mt-3 p-3 transition  delay-200 ease-linear duration-200 hover:bg-slate-300 ">Meu Painel</button>
                 </nav>
 
-                <button className="text-white mr-12 border border-solid 
+                <button className="text-white mr-12 border border-solid transition  delay-200 ease-linear duration-200
                 rounded-3xl w-28 mt-3 font-bold hover:bg-white hover:text-black">Acessar</button>
 
             </section>
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,14 @@
+
+import Head from "next/head"
+
+export default function Dashboard(){
+    return(
+        <>
+        <Head>
+            <title>Paigina Dashboard</title>
+        </Head>
+        <h1>Page Pailnel</h1>
+        </>
+    )
+
+}
